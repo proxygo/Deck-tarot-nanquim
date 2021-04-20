8d5ee4e21c376bb30376bec0b7b8563569ddeeed
Priya Nobre <53207638+PriyaNobre@users.noreply.github.com>
2021-04-19 23:54:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    var count = 0;
+    
+    $(".tarot-card").click(function() {
+      var card = $(this);
+      if(count < 4) {
+    
+       $(this).removeClass("top").addClass("bottom");
+        
+        count++;
+      }
+      if(count == 4) {
+       $("div").removeClass("bottom");
+        
+        count = 0;
+      }
+      shuffle(card);
+    });
+    });
+    
+    function shuffle(card) {
+        TweenLite.fromTo(
+            card, 
+            0.6, 
+            {
+            x:410, 
+            y:-15, 
+            ease: Expo.easeOut}, 
+            {
+            x:0,
+            y:0,
+            ease: Expo.easeIn});
+    };
